ef1b10fa70e7dca280b798826fcd1c20ea339d57
Charles <charlesbintha@gmail,com>
2026-06-29 23:07:06 +0000

    Generate sitemap XML without Blade

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -40,10 +40,36 @@ class SitemapController extends Controller
             ])
         );
 
-        $xml = view('seo.sitemap', compact('urls'))->render();
+        $xml = $this->buildSitemapXml($urls->all());
 
         return response($xml, 200, [
             'Content-Type' => 'application/xml; charset=UTF-8',
         ]);
+    }
+
+    protected function buildSitemapXml(array $urls): string
+    {
+        $lines = [
+            '<?xml version="1.0" encoding="UTF-8"?>',
+            '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        ];
+
+        foreach ($urls as $url) {
+            $lines[] = '  <url>';
+            $lines[] = '    <loc>'.$this->escapeXml($url['loc']).'</loc>';
+            $lines[] = '    <lastmod>'.$this->escapeXml($url['lastmod']).'</lastmod>';
+            $lines[] = '    <changefreq>'.$this->escapeXml($url['changefreq']).'</changefreq>';
+            $lines[] = '    <priority>'.$this->escapeXml($url['priority']).'</priority>';
+            $lines[] = '  </url>';
+        }
+
+        $lines[] = '</urlset>';
+
+        return implode("\n", $lines);
+    }
+
+    protected function escapeXml(string $value): string
+    {
+        return htmlspecialchars($value, ENT_XML1 | ENT_QUOTES, 'UTF-8');
     }
 }
